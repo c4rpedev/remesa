0894fbe8f068ec6e6d82f4763f2e80f03cdeeda5
alexandromag24 <34452436+alexandromag24@users.noreply.github.com>
2021-05-26 13:33:33 +0200

    styles added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { CardsComponent } from './shared/components/cards/cards.component';
+import { GoalsComponent } from './shared/components/goals/goals.component';
+import { TransactionsComponent } from './shared/components/transactions/transactions.component';
+import { OutcomeComponent } from './shared/components/outcome/outcome.component';
+import { BottomComponent } from './shared/components/bottom/bottom.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    SidebarComponent,
+    NavbarComponent,
+    CardsComponent,
+    GoalsComponent,
+    TransactionsComponent,
+    OutcomeComponent,
+    BottomComponent    
   ],
   imports: [
     BrowserModule,
