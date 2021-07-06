d471626df45c4b596499ae6e79624af19cf9553c
alexandromag24 <34452436+alexandromag24@users.noreply.github.com>
2021-07-06 19:00:34 +0200

    sidebar deleted fa icon

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { faChartLine } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
-  faChartLine = faChartLine;
+  
   isOpened: String;
   public screenWidth: any;
   public screenHeight: any;
