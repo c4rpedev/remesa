f9543ae4ea8a32467b87d1502d4d3fa70d670350
alexandromag24 <34452436+alexandromag24@users.noreply.github.com>
2021-07-20 12:13:27 +0200

    auth guard edited

--- src/app/core/services/auth.guard.ts
+++ src/app/core/services/auth.guard.ts
@@ -2,14 +2,13 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 
-import { AngularFireAuth } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
 
-  constructor(private router: Router, private afAuth: AngularFireAuth){
+  constructor(private router: Router){
 
   }
 
@@ -18,19 +17,7 @@ export class AuthGuard implements CanActivate {
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
 
     return new Promise((resolve, reject) => {
-        this.afAuth.onAuthStateChanged((user) => {
-            if (user) {
-
-                // if (!user.emailVerified)                            // if the user hasn't verified their email, send them to that page
-                //     this.router.navigate(['/verify-email']);
-
-                resolve(true);
-            } else {
-                console.log('Auth Guard: user is not logged in');
-                this.router.navigate(['/login']);                   // a logged out user will always be sent to home
-                resolve(false);
-            }
-        });
+      
     });
 }
   
