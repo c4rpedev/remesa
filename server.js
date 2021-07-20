ca7a273cf6f9b21046ed61ac9b00be02677cce43
alexandromag24 <34452436+alexandromag24@users.noreply.github.com>
2021-07-20 18:07:37 +0200

    trying to fix server.js

--- server.js
+++ server.js
@@ -5,6 +5,24 @@ const path = require('path');
 
 const app = express();
 app.use(cors());
+
+
+const whitelist = ['https://buttymanager.herokuapp.com']; // list of allow domain
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin) {
+            return callback(null, true);
+        }
+
+        if (whitelist.indexOf(origin) === -1) {
+            var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/buttymanager'));
 
