200709d3129f84b8145e2b8f585581ce8efe8049
alexandromag24 <34452436+alexandromag24@users.noreply.github.com>
2021-07-22 15:26:44 +0200

    report modified

--- src/app/pages/report/report.component.ts
+++ src/app/pages/report/report.component.ts
@@ -15,68 +15,67 @@ export class ReportComponent implements AfterViewInit {
   orders= new Array<Order>();
   pivotGridDataSource: any;
 
-  constructor() {
+  constructor(private orderService: OrderService) {
     // this.customizeTooltip = this.customizeTooltip.bind(this);
 
     
   }
 
   ngAfterViewInit() {
-    // this.orderService.getOrderSucursal('buttymanager').then(res=>{      
-    //   for (let order of res) {
-    //     console.log('Order');
-    //     this.orders.push(order.attributes);
-    //   }
-    //   this.pivotGridDataSource = {
-    //     fields: [{
-    //       caption: "Agencia",
-    //       width: 120,
-    //       dataField: "orderAgency",
-    //       area: "row",
-    //       sortBySummaryField: "Total"
-    //     },{
-    //       caption: "Provincia",
-    //       width: 120,
-    //       dataField: "orderProvince",
-    //       area: "row",          
-    //     }, {
-    //       caption: "Municipio",
-    //       dataField: "orderMunicipio",
-    //       width: 150,
-    //       area: "row"
-    //     }, {
-    //       dataField: "createdAt",
-    //       dataType: "date",
-    //       area: "column"
-    //     },{
-    //       groupName: "date",
-    //       groupInterval: "month",
-    //       visible: false
-    //     },{
-    //       caption: "Cantidad de Ordenes",
-    //       summaryType: "count",
-    //       area: "data"
-    //     },{
-    //       caption: "Total",
-    //       dataField: "orderPrice",
-    //       dataType: "number",
-    //       summaryType: "sum",
-    //       format: "currency",
-    //       area: "data"
-    //     }],
-    //     store: this.orders
-    //   }                 
-    // }); 
-    // this.pivotGrid.instance.bindChart(this.chart.instance, {
-    //   dataFieldsDisplayMode: "splitPanes",
-    //   alternateDataFields: false
-    // });
+    this.orderService.getOrderSucursal('buttymanager').then(res=>{      
+      for (let order of res) {
+        console.log('Order');
+        this.orders.push(order.attributes);
+      }
+      this.pivotGridDataSource = {
+        fields: [{
+          caption: "Agencia",
+          width: 120,
+          dataField: "orderAgency",
+          area: "row",
+          sortBySummaryField: "Total"
+        },{
+          caption: "Provincia",
+          width: 120,
+          dataField: "orderProvince",
+          area: "row",          
+        }, {
+          caption: "Municipio",
+          dataField: "orderMunicipio",
+          width: 150,
+          area: "row"
+        }, {
+          dataField: "createdAt",
+          dataType: "date",
+          area: "column"
+        },{
+          groupName: "date",
+          groupInterval: "month",
+          visible: false
+        },{
+          caption: "Cantidad de Ordenes",
+          summaryType: "count",
+          area: "data"
+        },{
+          caption: "Total",
+          dataField: "orderPrice",
+          dataType: "number",
+          summaryType: "sum",
+          format: "currency",
+          area: "data"
+        }],
+        store: this.orders
+      }                 
+    }); 
+    this.pivotGrid.instance.bindChart(this.chart.instance, {
+      dataFieldsDisplayMode: "splitPanes",
+      alternateDataFields: false
+    });
 
-    // setTimeout(() => {
-    //     var dataSource = this.pivotGrid.instance.getDataSource();
-    //     dataSource.expandHeaderItem('row', ['North America']);
-    //     dataSource.expandHeaderItem('column', [2013]);
-    // }, 0);
+    setTimeout(() => {
+        var dataSource = this.pivotGrid.instance.getDataSource();
+       
+    }, 0);
   }
 
   // customizeTooltip(args:any) {
