a6fdee3bffe6058306c2434dbfd47fb4fe8f1584
alexandromag24 <34452436+alexandromag24@users.noreply.github.com>
2021-08-14 15:11:26 +0200

    repair add order

--- src/app/pages/order/edit-order/edit-order.component.ts
+++ src/app/pages/order/edit-order/edit-order.component.ts
@@ -78,17 +78,17 @@ export class EditOrderComponent implements OnInit {
 }
   onSubmit(form: NgForm){
     // var albaranes = 'albaranes.jpg'
-    // var hasAlbaran = false;
+    var hasAlbaran = false;
     console.log(form);
     
     if(form.valid || form.disabled){
-      // if((this.order.state != 'Finalizado' && this.order.state != 'Nuevo' && this.order.state != 'Revisado') && this.order.orderAlbaran.name.indexOf(albaranes, this.order.orderAlbaran.name.length - albaranes.length) !== -1){
-      //   hasAlbaran = true
+      if( this.order.state != 'Nuevo' && this.order.state != 'Revisado' && this.order.state != 'En Proceso'){
+        hasAlbaran = true
 
-      // }
+       }
       console.log(this.order.orderSucursal);
       
-      this.orderService.updateOrder(this.order, this.orderId, this.img.toString());
+      this.orderService.updateOrder(this.order, this.orderId, this.img.toString(), hasAlbaran);
       Swal.fire({
         position: 'top-end',
         icon: 'success',
